aa0f21b59e39ddbf08192af991368d0a655f1b6a
Bao Gia <nguyenducgiabao18@outlook.com>
2024-12-09 20:01:37 +0700

    Add project files.

---
+++ bai2/Controllers/CategoryController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bai2.Models;
+
+namespace bai2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private static List<Category> categories = new List<Category>();
+        private static int nextId = 1;
+
+        // GET: Category
+        public IActionResult Index()
+        {
+            return View(categories);
+        }
+
+        // GET: Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Id = nextId++;
+                categories.Add(category);
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Category/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var category = categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingCategory = categories.FirstOrDefault(c => c.Id == category.Id);
+                if (existingCategory != null)
+                {
+                    existingCategory.Name = category.Name;
+                }
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Category/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var category = categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var category = categories.FirstOrDefault(c => c.Id == id);
+            if (category != null)
+            {
+                categories.Remove(category);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ bai2/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using bai2.Models;
+
+namespace bai2.Repositories
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAllCategories();
+    }
+}
